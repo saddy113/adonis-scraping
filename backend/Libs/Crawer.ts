6feaa036e3c954e7189209bb933433746a121e3c
sek <settawut.th@gmail.com>
2021-07-06 19:44:17 +0700

    add - docker compose production

--- backend/Libs/Crawer.ts
+++ backend/Libs/Crawer.ts
@@ -3,7 +3,6 @@ import puppeteer from 'puppeteer'
 export default class Crawer {
   async config() {
     const browserFetcher = await puppeteer.createBrowserFetcher()
-    // const browserFetcher = await puppeteer
     const revisionInfo = await browserFetcher.download('818858')
     const browser = await puppeteer.launch({
       executablePath: revisionInfo.executablePath,
